2be9e3b3623480e572aa1b289e888c0d9eca9d30
baldulin <baldulin@systemli.org>
2022-01-26 15:58:31 +0100

    Revert "fix: Update width/height after changing full screen state"
    This reverts commit 4e10d751b56570fe63d0148439403270c4185469.

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -936,10 +936,8 @@ export class Map extends Component<MapProps, MapReactState> {
   handleFullscreen = (event: Event): void => {
       if (document.fullscreenElement === this._containerRef) {
         this.setState({isFullscreen: true});
-        this.updateWidthHeight();
       } else {
         this.setState({isFullscreen: false});
-        this.updateWidthHeight();
       }
   }
 
